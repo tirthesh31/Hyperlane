58d2995b1c29f3a1a43451088619d752fc6c80df
tirthesh31 <tirtheshmotiwala@gmail.com>
2025-05-02 12:41:41 -0400

    refining code

--- i-v1.64.js
+++ i-v1.64.js
@@ -16,7 +16,7 @@
         { id: 'star2', x: viewportWidth * .24, y: viewportHeight * .405, name: 'OPTIMISM' },
         { id: 'star3', x: viewportWidth * .65, y: viewportHeight * .2, name: 'UNICHAIN' },
         { id: 'star4', x: viewportWidth * .65, y: viewportHeight * .415, name: 'SOLANA' },
-        { id: 'star5', x: viewportWidth * .50, y: viewportHeight * .3 , name: 'ETHEREUM' }
+        { id: 'star5', x: viewportWidth * .488, y: viewportHeight * .32 , name: 'ETHEREUM' }
       ];
     } else {
       starSystems = [
